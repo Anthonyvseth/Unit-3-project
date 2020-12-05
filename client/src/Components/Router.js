0faec760c3c5f75c20d7372393a8a5bcad770ade
MICHAEL <MJENNINGS6@gmail.com>
2020-12-05 16:45:44 -0500

    added SignIn/Up to Router

--- client/src/Components/Router.js
+++ client/src/Components/Router.js
@@ -1,19 +1,45 @@
-import React from 'react';
+import React from "react";
+import { Switch, Route, withRouter } from "react-router-dom";
 
-import LandingPage from '../pages/LandingPage'
-
+import LandingPage from "../Pages/LandingPage";
+import Home from "../Pages/Home";
+import SignUp from "../Components/SignUp";
+import SignIn from "../Components/SignIn";
 
 const Router = () => {
+  return (
+    <main>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          component={(props) => (
+            <LandingPage>
+              <Home {...props} />
+            </LandingPage>
+          )}
+        />
+        <Route
+          exact
+          path="/login"
+          component={(props) => (
+            <LandingPage>
+              <SignIn {...props} />
+            </LandingPage>
+          )}
+        />
+        <Route
+          exact
+          path="/register"
+          component={(props) => (
+            <LandingPage>
+              <SignUp {...props} />
+            </LandingPage>
+          )}
+        />
+      </Switch>
+    </main>
+  );
+};
 
-
-    return (
-        <main>
-            
-            <LandingPage />
-
-        </main>
-    )
-
-}
-
-export default Router
+export default withRouter(Router);
