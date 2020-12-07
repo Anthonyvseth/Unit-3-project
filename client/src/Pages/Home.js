bddbe08dc12084f80a8e8247feb3d39a1fcd84a2
Anthonyvseth <62005175+Anthonyvseth@users.noreply.github.com>
2020-12-07 15:12:07 -0700

    pulling from main

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -11,7 +11,7 @@ export default (props) => {
   return (
     <div className='landing-container'>
       <h1>Make Your Day</h1>
-      {/* <h2> Welcome, {props.account.firstName}</h2> */}
+      <h2> Welcome, {props.account.firstName}</h2>
       <div className='landing-btns'>
         <Button
           className='btns'
