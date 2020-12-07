f941e876ae2d2c548ffd8a5a623be45e584e5577
MICHAEL <MJENNINGS6@gmail.com>
2020-12-07 16:05:08 -0500

    passed state for account

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -3,12 +3,12 @@ import Button from '../components/Button';
 import DateTime from '../components/DateTime';
 
 export default (props) => {
-  console.log('home props: ', props.account);
+  // console.log('home props: ', props.account);
 
   return (
     <div className='landing-container'>
       <h1>Make Your Day</h1>
-      <h2> Welcome, {props.account.firstName}</h2>
+      <h2> Welcome, {props.account.firstName} </h2>
       <div className='landing-btns'>
         <Button
           className='btns'
