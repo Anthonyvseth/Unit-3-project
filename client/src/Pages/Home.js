c549366588a75e4c41ab4616e4a1f132a07f7eda
Anthonyvseth <62005175+Anthonyvseth@users.noreply.github.com>
2020-12-07 09:15:00 -0700

    clock working on home

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,20 +1,14 @@
 import React from "react";
 import Button from "../components/Button";
+import DateTime from '../components/DateTime'
 
 export default (props) => {
   return (
     <div className="landing-container">
       <h1>Make Your Day</h1>
-      <p>lorem ipsum dsfj woeiaslkdj skdf asdkjfasdlkjsdjf kaslf</p>
-      <div className="landing-btns">
-        <Button
-          className="btns"
-          buttonStyle="btn--outline"
-          buttonSize="btn--large"
-        >
-          add Focus
-        </Button>
-      </div>
+      <h1>
+        <DateTime></DateTime>
+      </h1>
     </div>
   );
 };
