e4e21290e351cd013e78445dfe5899a53f09ef08
Mike Zahuta <mike.zahuta@gmail.com>
2020-12-08 13:02:55 -0500

    working focus.js, fix updatefocus function accountservices.js, import on home

--- client/src/Components/Focus.js
+++ client/src/Components/Focus.js
@@ -1,49 +1,39 @@
-import React, { useState } from "react";
-import TextInput from "./TextInput";
+import React, { useState, useEffect } from "react";
 import { __UpdateFocus } from "../services/AccountService";
 
 const Focus = (props) => {
-  const [focusValue, setFocusValue] = useState("");
+  console.log('focus props:', props.account)
+  const [focusValue, setFocusValue] = useState(null);
 
-  //   const focusField = (e) => {
-  //     e.preventDefault();
-  //     setFocusValue(e.target.value);
-  //     console.log("Focus:", focusValue);
-  //   };
+  useEffect(()=>{
+    console.log(`HIT FocusUE:`)
+    if (focusValue === null) {
+      setFocusValue(props.account.focus)
+    }
+  })
 
   const handleSubmit = async (e) => {
-    console.log("MZ HIT - Focus.HandleSubmit:", e.target.value);
     e.preventDefault();
+    console.log("MZ HIT - Focus.HandleSubmit:", e.target.value);
     try {
       //   const newFocus = { focus: focusValue };
-      const updatedFocus = await __UpdateFocus(focusValue);
+      console.log('focusValue:',focusValue)
+      const sentValues = {focus: focusValue}
+      const updatedFocus = await __UpdateFocus(sentValues);
       console.log("Focus updated:", updatedFocus);
     } catch (error) {
       throw error;
     }
   };
 
-  const handleChange = async (e) => {
+  const handleChange = (e) => {
+    e.preventDefault();
     console.log("MZ HIT Focus.handleChange:", e.target.value);
-    e.preventDefault();
-    try {
-      const updatedFocus = await __UpdateFocus(e.targetValue);
-      console.log("MZ within handleChange; Focus updated:", updatedFocus);
-      setFocusValue(updatedFocus);
-    } catch (error) {
-      throw error;
-    }
-  };
-
-  const handleClick = (e) => {
-    console.log("MZ HIT Focus.handleClick:", e.target.value);
-    //this is what is firing with either an enter keystroke or click
-    e.preventDefault();
-    setFocusValue(e.target.value);
+    setFocusValue(e.target.value)   
   };
 
   const clearFocus = (e) => {
-    setFocusValue('')
+   console.log(e.target.value)
   }
 
   if (props.account === null || props.account === undefined) {
@@ -51,13 +41,14 @@ const Focus = (props) => {
   } else if (props.account.focus !== 'NULL' || props.account.focus !== "") {
     return (
         <form onSubmit={(e) => handleSubmit(e)}>
-          <TextInput
+          <input
+            type='text'
             name="focus"
-            value={props.account.focus}
+            value={focusValue}
             placeholder="Focus for today?"
-            onChange={(e) => handleChange(e)}
+            onChange={handleChange}
           />
-          <button onClick={(e) => handleClick(e)}>Add</button>
+          <button>Add</button>
         </form>
     )
   } else {
--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -3,9 +3,10 @@ import Button from '../components/Button';
 import DateTime from '../components/DateTime';
 import SignOut from '../components/SignOut'
 import TodoList from '../components/Todo/TodoList'
+import Focus from '../components/Focus'
 
 export default (props) => {
-  console.log('home props: ', props.account);
+  //console.log('home props: ', props.account);
   const { onClickSignOut } = props
 
   return (
@@ -13,13 +14,7 @@ export default (props) => {
       <h1>Make Your Day</h1>
       <h2> Welcome, {props.account.firstName}</h2>
       <div className='landing-btns'>
-        <Button
-          className='btns'
-          buttonStyle='btn--outline'
-          buttonSize='btn--large'
-        >
-          add Focus
-        </Button>
+      <Focus account={props.account}/>
       </div>
       <h1>
         <DateTime></DateTime>
