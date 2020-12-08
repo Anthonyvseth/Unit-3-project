c604b1021830b04933cb4084755007dfa89779b1
Ted Fossum <theodorefossum@gmail.com>
2020-12-08 13:05:32 -0500

    Display weather data on home page. Fix bug where SignUp and SignIn now take user to the home page.

--- controllers/OpenWeather.js
+++ controllers/OpenWeather.js
@@ -6,8 +6,8 @@ const ApiClient = axios.create({
 });
 
 const getCurrentWeather = async (zipCode) => {
-    console.log('zipCode: ', zipCode)
-    const weatherURL = `/weather?zip=${zipCode},us&appid=17d8f8b0577bc4046466859bae5da432`
+    console.log('******zipCode: ', zipCode)
+    const weatherURL = `/weather?zip=${zipCode},us&units=imperial&appid=17d8f8b0577bc4046466859bae5da432`
     try {
         const res = await ApiClient.get(weatherURL)
         console.log('Service: ', res.data)
