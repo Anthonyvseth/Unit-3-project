ed1d7049d21e84f0fa42da798f5cc3038614fd25
Mike Zahuta <mike.zahuta@gmail.com>
2020-12-09 23:50:43 -0500

    code fix conditional rendering Focus, add FontAwesome package, style buttons on Focus

--- client/src/Components/Focus.js
+++ client/src/Components/Focus.js
@@ -1,22 +1,25 @@
 import React, { useState, useEffect } from "react";
 import { __UpdateFocus } from "../services/AccountService";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheckSquare, faPlusSquare } from "@fortawesome/free-solid-svg-icons";
+
 
 const Focus = (props) => {
-  const { setNeedsRefresh } = props
+  const { setNeedsRefresh } = props;
   const [focusValue, setFocusValue] = useState(null);
 
-  useEffect(()=>{
+  useEffect(() => {
     if (focusValue === null) {
-      setFocusValue(props.account.focus)
+      setFocusValue(props.account.focus);
     }
-  })
+  });
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const sentValues = {focus: focusValue}
+      const sentValues = { focus: focusValue };
       const updatedFocus = await __UpdateFocus(sentValues);
-      setNeedsRefresh(true)
+      setNeedsRefresh(true);
     } catch (error) {
       throw error;
     }
@@ -24,43 +27,56 @@ const Focus = (props) => {
 
   const handleChange = (e) => {
     e.preventDefault();
-    setFocusValue(e.target.value)   
+    setFocusValue(e.target.value);
   };
 
   const clearFocus = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     try {
-      const updatedFocus = await __UpdateFocus({focus: ''})
-      setNeedsRefresh(true)
+      const updatedFocus = await __UpdateFocus({ focus: "" });
+      setNeedsRefresh(true);
     } catch (error) {
       throw error;
     }
-  }
+  };
 
   if (props.account === null || props.account === undefined) {
     return null;
-  } else if (props.account.focus !== 'NULL' || props.account.focus !== "") {
+  } else if (props.account.focus === "NULL" || props.account.focus === "") {
     return (
-          <div>
+      <div>
         <form onSubmit={(e) => handleSubmit(e)}>
           <input
-            type='text'
+            type="text"
             name="focus"
             value={focusValue}
             placeholder="Focus for today?"
             onChange={handleChange}
           />
-          <button>Add</button>
-        </form>
-        <button onClick={(e)=>clearFocus(e)}>Clear</button>
-        </div>
-    )
+          <FontAwesomeIcon 
+          className="fas fa-white"
+          icon={faPlusSquare}
+          />
+          <FontAwesomeIcon
+            className="fas fa-white"
+            icon={faCheckSquare}
+            onClick={clearFocus}
+            />
+          </form>
+      </div>
+    );
   } else {
-      return (
+    return (
       <div>
-        <h3>You're currently focused on:</h3>
-        <p>{props.account.focus}</p>
-        <button onClick={clearFocus}>*</button>
+        <h3>Your current focus:</h3>
+        <p>
+          {props.account.focus}
+          <FontAwesomeIcon
+            className="fas fa-white"
+            icon={faCheckSquare}
+            onClick={clearFocus}
+          />
+        </p>
       </div>
     );
   }
