5de69858893b275e54524d6e5ac9f0d1b259a74f
Anthonyvseth <62005175+Anthonyvseth@users.noreply.github.com>
2020-12-10 18:12:50 -0700

    Button on signup and sign in fixed but not sign out

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,62 +1,48 @@
 import React from 'react';
+
 import DateTime from '../components/DateTime';
 import SignOut from '../components/SignOut';
 import WebLinks from '../components/WebLink/WebLinks';
 import Todos from '../components/Todo/Todos';
 import Focus from '../components/Focus';
 import Weather from '../components/Weather';
-import '../styles/Home.css';
-
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 
 export default (props) => {
   //console.log('home props: ', props.account);
   const { account, onClickSignOut, setNeedsRefresh } = props;
 
   if (account !== null && account !== undefined) {
-    return (
-      <div>
-        <div>
-          <Container fluid>
-            <Row>
-              <Col md={{ span: 1 }}>
-                <div>
-                  <WebLinks {...props} setNeedsRefresh={setNeedsRefresh} />
-                </div>
-              </Col>
-              <Col md={{ span: 1, offset: 10 }}>
-                <div>
-                  <Todos // we are going to need to send todolist as props
-                    account={account}
-                    setNeedsRefresh={setNeedsRefresh}
-                  />
-                </div>
-              </Col>
-            </Row>
-          </Container>
-        </div>
-        <div className='Home-container'>
-          <h1>Make Your Day</h1>
-          <h2> Welcome, {props.account.firstName}</h2>
-          <div className='landing-btns'>
-            <Focus account={props.account} setNeedsRefresh={setNeedsRefresh} />
-          </div>
-          <h1>
-            <DateTime></DateTime>
-          </h1>
 
-          <div>
-            <SignOut onClick={onClickSignOut} />
-          </div>
-          <div>
-            <Weather />
-          </div>
+    return (
+      <div className='landing-container'>
+        <h1>Make Your Day</h1>
+        <div>
+          <Weather />
+        </div>
+        <h2> Welcome, {props.account.firstName}</h2>
+        <div className='landing-btns'>
+          <Focus account={props.account} />
+        </div>
+        <h1>
+          <DateTime></DateTime>
+        </h1>
+        <div>
+          <WebLinks
+            {...props}
+            setNeedsRefresh={setNeedsRefresh} />
+        </div>
+        <div>
+          <Todos // we are going to need to send todolist as props
+            account={account}
+            setNeedsRefresh={setNeedsRefresh}
+          />
+          <SignOut onClick={onClickSignOut} />
         </div>
       </div>
-    );
+    )
   } else {
-    <div>Loading...</div>;
+    <div>
+      Loading...
+  </div>
   }
 };
