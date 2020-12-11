00d9e8c739959173b6d3eb6bf66f749ca8c418e2
Ted Fossum <theodorefossum@gmail.com>
2020-12-11 16:55:16 -0500

    Fix import path for styles.

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/index.css';
 import './styles/SignUp.css'
 import './styles/Button.css'
